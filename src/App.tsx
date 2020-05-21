7a582e1b5ab7ddaebcd76a1972c1ca708e1beec4
Dmitriy Prone <idimzz@gmail.com>
2020-05-21 18:47:45 +0700

    update core styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,17 @@
-import React, { FC } from 'react';
-import { useQuery } from '@apollo/react-hooks';
+import React from 'react';
 import styled from 'styled-components';
-import { CHARACTERS } from './gql-operations/queries';
-import { Characters, CharactersVariables } from './gql-operations/types/Characters';
 
 const StyledApp = styled.div`
   display: flex;
   justify-content: center;
+  padding-top: 141px;
+  font: inherit;
 `;
-const App: FC = () => {
-  const { loading, error, data } = useQuery<Characters, CharactersVariables>(CHARACTERS, {
-    variables: { page: 2, filter: {} }
-  });
-  const getContent = (children: JSX.Element | string): JSX.Element => (
-    <StyledApp>
-      <p>
-        {children}
-      </p>
-    </StyledApp>
-  );
 
-  if (loading) return getContent('Loading...');
-  if (error) return getContent(`Error! ${error}`);
-
-  return getContent(`Result: characters count is ${data?.characters?.info?.count}`);
-};
-
-export default App;
+export default (): JSX.Element => (
+  <StyledApp>
+    <p>
+      Test
+    </p>
+  </StyledApp>
+);
