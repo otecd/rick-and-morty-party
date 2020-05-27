2028b89f5a972a6cff5da05ee1c321a3e0a4af78
Dmitriy Prone <idimzz@gmail.com>
2020-05-27 17:34:40 +0700

    add test fixtures

--- src/store/finder/actions.test.ts
+++ src/store/finder/actions.test.ts
@@ -1,12 +1,6 @@
 import { Dispatch } from 'react';
+import { name, errorMessage } from '../../test-utils/fixtures';
 import actions, { finderActionTypes } from './actions';
-
-let dispatch: Dispatch<ReducerAction>;
-const payload = 'test';
-
-beforeEach(() => {
-  dispatch = jest.fn();
-});
 
 test('should have correct methods names', () => {
   expect(actions).toMatchObject({
@@ -18,37 +12,52 @@ test('should have correct methods names', () => {
     stopLoading: expect.any(Function),
   });
 });
-test('should dispatch correct reducer actions on updateName', () => {
-  expect(actions.updateName(dispatch, payload)).toBeUndefined();
-  expect(dispatch).toBeCalledTimes(2);
-  expect(dispatch).toBeCalledWith({ type: finderActionTypes.NAME_UPDATED, payload });
-  expect(dispatch).toBeCalledWith({ type: finderActionTypes.LOADING_STARTED });
-});
-test('should dispatch correct reducer actions on updateNameTyped', () => {
-  expect(actions.updateNameTyped(dispatch, payload)).toBeUndefined();
-  expect(dispatch).toBeCalledTimes(2);
-  expect(dispatch).toBeCalledWith({ type: finderActionTypes.NAME_TYPED_UPDATED, payload });
-  expect(dispatch).toBeCalledWith({ type: finderActionTypes.ERROR_CLEARED });
-});
-test('should dispatch correct reducer actions on throwError', () => {
-  expect(actions.throwError(dispatch, payload)).toBeUndefined();
-  expect(dispatch).toBeCalledTimes(3);
-  expect(dispatch).toBeCalledWith({ type: finderActionTypes.ERROR_THROWN, payload });
-  expect(dispatch).toBeCalledWith({ type: finderActionTypes.NAME_UPDATED, payload: '' });
-  expect(dispatch).toBeCalledWith({ type: finderActionTypes.LOADING_ENDED });
-});
-test('should dispatch correct reducer actions on clearError', () => {
-  expect(actions.clearError(dispatch)).toBeUndefined();
-  expect(dispatch).toBeCalledTimes(1);
-  expect(dispatch).toBeCalledWith({ type: finderActionTypes.ERROR_CLEARED });
-});
-test('should dispatch correct reducer actions on startLoading', () => {
-  expect(actions.startLoading(dispatch)).toBeUndefined();
-  expect(dispatch).toBeCalledTimes(1);
-  expect(dispatch).toBeCalledWith({ type: finderActionTypes.LOADING_STARTED });
-});
-test('should dispatch correct reducer actions on stopLoading', () => {
-  expect(actions.stopLoading(dispatch)).toBeUndefined();
-  expect(dispatch).toBeCalledTimes(1);
-  expect(dispatch).toBeCalledWith({ type: finderActionTypes.LOADING_ENDED });
+
+describe('should dispatch correct reducer actions on method call', () => {
+  let dispatch: Dispatch<ReducerAction>;
+
+  beforeEach(() => {
+    dispatch = jest.fn();
+  });
+
+  test('should dispatch correct reducer actions on updateName', () => {
+    const payload = name;
+
+    expect(actions.updateName(dispatch, payload)).toBeUndefined();
+    expect(dispatch).toBeCalledTimes(2);
+    expect(dispatch).toBeCalledWith({ type: finderActionTypes.NAME_UPDATED, payload });
+    expect(dispatch).toBeCalledWith({ type: finderActionTypes.LOADING_STARTED });
+  });
+  test('should dispatch correct reducer actions on updateNameTyped', () => {
+    const payload = name;
+
+    expect(actions.updateNameTyped(dispatch, payload)).toBeUndefined();
+    expect(dispatch).toBeCalledTimes(2);
+    expect(dispatch).toBeCalledWith({ type: finderActionTypes.NAME_TYPED_UPDATED, payload });
+    expect(dispatch).toBeCalledWith({ type: finderActionTypes.ERROR_CLEARED });
+  });
+  test('should dispatch correct reducer actions on throwError', () => {
+    const payload = errorMessage;
+
+    expect(actions.throwError(dispatch, payload)).toBeUndefined();
+    expect(dispatch).toBeCalledTimes(3);
+    expect(dispatch).toBeCalledWith({ type: finderActionTypes.ERROR_THROWN, payload });
+    expect(dispatch).toBeCalledWith({ type: finderActionTypes.NAME_UPDATED, payload: '' });
+    expect(dispatch).toBeCalledWith({ type: finderActionTypes.LOADING_ENDED });
+  });
+  test('should dispatch correct reducer actions on clearError', () => {
+    expect(actions.clearError(dispatch)).toBeUndefined();
+    expect(dispatch).toBeCalledTimes(1);
+    expect(dispatch).toBeCalledWith({ type: finderActionTypes.ERROR_CLEARED });
+  });
+  test('should dispatch correct reducer actions on startLoading', () => {
+    expect(actions.startLoading(dispatch)).toBeUndefined();
+    expect(dispatch).toBeCalledTimes(1);
+    expect(dispatch).toBeCalledWith({ type: finderActionTypes.LOADING_STARTED });
+  });
+  test('should dispatch correct reducer actions on stopLoading', () => {
+    expect(actions.stopLoading(dispatch)).toBeUndefined();
+    expect(dispatch).toBeCalledTimes(1);
+    expect(dispatch).toBeCalledWith({ type: finderActionTypes.LOADING_ENDED });
+  });
 });
--- src/store/party/actions.test.ts
+++ src/store/party/actions.test.ts
@@ -1,32 +1,25 @@
 import { Dispatch } from 'react';
+import { item, role } from '../../test-utils/fixtures';
 import actions, { partyActionTypes } from './actions';
-import { NAME_MORTY } from '../../const';
 
 let dispatch: Dispatch<ReducerAction>;
-const payload: {
-  item: Item;
-  role: PartyRole;
-} = {
-  item: {
-    __typename: 'Character',
-    id: 'id',
-    name: 'name',
-    image: 'image',
-  },
-  role: NAME_MORTY,
-};
-
-beforeEach(() => {
-  dispatch = jest.fn();
-});
 
 test('should have correct methods names', () => {
   expect(actions).toMatchObject({
     admitMember: expect.any(Function),
   });
 });
-test('should dispatch correct reducer actions on admitMember', () => {
-  expect(actions.admitMember(dispatch, payload)).toBeUndefined();
-  expect(dispatch).toBeCalledTimes(1);
-  expect(dispatch).toBeCalledWith({ type: partyActionTypes.MEMBER_ADMITED, payload });
+
+describe('should dispatch correct reducer actions on method call', () => {
+  beforeEach(() => {
+    dispatch = jest.fn();
+  });
+
+  test('admitMember', () => {
+    const payload = { item, role };
+
+    expect(actions.admitMember(dispatch, payload)).toBeUndefined();
+    expect(dispatch).toBeCalledTimes(1);
+    expect(dispatch).toBeCalledWith({ type: partyActionTypes.MEMBER_ADMITED, payload });
+  });
 });
